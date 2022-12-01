71a3e09a1ba342a12a8fcd7f377ec40393d44ad1
unnc-fse-20217748 <scypi1@nottingham.edu.cn>
2022-12-01 16:17:45 +0700

    Update GUI for 2nd Screen

--- GUI/src/main/java/com/example/demo6/HelloApplication.java
+++ GUI/src/main/java/com/example/demo6/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.demo6;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,9 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Space Asteroids");
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        // FXMLLoader fxmlLoader = new
+        // FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        // stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
--- GUI/src/main/java/com/example/demo6/HelloController.java
+++ GUI/src/main/java/com/example/demo6/HelloController.java
@@ -1,18 +1,47 @@
 package com.example.demo6;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class HelloController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("page2.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     Label label = new Label("Space Asteroids");
     Button btnStart = new Button("Start");
     Button btnExit = new Button("Quit");
-    Group root = new Group();
+    // Group root = new Group();
     @FXML
     private ChoiceBox<?> Dropdownmenu;
 
@@ -31,10 +60,14 @@ public class HelloController {
         System.exit(0);
     }
 
-    @FXML
-    void StartPressed(ActionEvent event) {
-        System.out.println("Start pressed");
-    }
+    /*
+     * @FXML
+     * void StartPressed(ActionEvent event) throws IOException {
+     * // System.out.println("Start pressed");
+     * AnchorPane pane = FXMLLoader.load(getClass().getResource("page2.fxml"));
+     * //root.getChildren().setAll(pane);
+     * }
+     */
 
     @FXML
     void dropdownmenuclicked(MouseEvent event) {
